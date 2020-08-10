f93249ab044632af3f5acb81640ce7842396f6cd
madmodder123 <33565771+madmodder123@users.noreply.github.com>
2020-08-10 12:52:59 -0700

    Update id_in.cpp

--- src/id_in.cpp
+++ src/id_in.cpp
@@ -747,26 +747,6 @@ SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt");
 				JoySensitivity = new JoystickSens[JoyNumAxes];
 			}
 		}
-#if SDL_VERSION_ATLEAST(2,0,0)
-			if(GameController)
-			{
-				SDL_JoystickGUID guid = SDL_JoystickGetGUID(Joystick);
-				char guid_str[1024];
-				SDL_JoystickGetGUIDString(guid, guid_str, sizeof(guid_str));
-				
-				if (cin >> guid_str && 
-				std::find(std::begin(xbox_guids), std::end(xbox_guids), guid_str) == std::end(xbox_guids))
-				{
-				printf("triggers_to_buttons is enabled, fixing gamemapping!");
-				// create default mapping - this is the PS3 dual shock mapping
-				std::string mapping = string(guid_str) + "," + string(SDL_GameControllerName(GameController)) + ",a:b0,b:b1,back:b8,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b10,leftshoulder:b4,leftstick:b11,lefttrigger:b6,leftx:a0,lefty:a1,rightshoulder:b5,rightstick:b12,righttrigger:b7,rightx:a2,righty:a3,start:b9,x:b2,y:b3,platform:Linux,";
-				SDL_GameControllerAddMapping(mapping.c_str());
-				} else {
-				printf("triggers_to_buttons is disabled! Carry on!");
-				}
-
-			}
-#endif
 		if(JoySensitivity)
 		{
 			for(int i = 0;i < JoyNumAxes;++i)
